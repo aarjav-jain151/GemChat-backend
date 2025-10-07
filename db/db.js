d54aa015b7f2322210661fc1aecea59b3dd8025d
aarjav-jain151 <aarjav-jain151@users.noreply.github.com>
2025-10-07 17:12:31 +0530

    Final fix: Increased Mongoose timeouts for stable free-tier connection

--- db/db.js
+++ db/db.js
@@ -1,14 +1,16 @@
 import mongoose from "mongoose";
 
-
 function connect() {
-    mongoose.connect(process.env.MONGODB_URI)
-        .then(() => {
-            console.log("Connected to MongoDB");
-        })
-        .catch(err => {
-            console.log(err);
-        })
+    mongoose.connect(process.env.MONGODB_URI, {
+        serverSelectionTimeoutMS: 50000, 
+        socketTimeoutMS: 50000,
+    })
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch(err => {
+        console.log("DB connection error:", err);
+    })
 }
 
 export default connect;
